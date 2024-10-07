7bf01142ac4075ae9f2c80102637cdd2e2f59a70
stevan-nomoq <stevan@nomoq.com>
2024-10-07 13:08:44 +0200

    init

---
+++ nomoq-test-module/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Nomoq Test'
+    'version': '0.1',
+    'summary': 'Nomoq test',
+    'description': 'test',
+    'sequence': 1,
+    'author': 'nomoq',
+    'website': 'http://nomoq.com',
+    'depends': ['base'],
+i   'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'LGPL-3'
+}
